608e8f685ab2f68989a05a83eed61579e097e734
wilsoncharles <wilsoncharles21@gmail.com>
2022-10-28 19:14:14 +0100

    connection file config

---
+++ sensor/configuration/mongo_db_connection.py
@@ -0,0 +1,19 @@
+import pymongo
+from sensor.constant.database import DATABASE_NAME
+import certifi
+ca = certifi.where()
+
+
+class MongoDBClient:
+    client = None
+    def __init__(self,database_name=DATABASE_NAME) -> None:
+        try:
+            if MongoDBClient.client is None:
+                mongo_db_url = "mongodb+srv://mlopscluster:lassRO2q61NIY76h@mlops.ij75lqf.mongodb.net/?retryWrites=true&w=majority"
+                MongoDBClient.client = pymongo.MongoClient(mongo_db_url,tlsCAFile=ca)
+            self.client = MongoDBClient.client
+            self.database = self.client[database_name]
+            self.database_name = database_name
+        except Exception as e:
+            raise e
+    
