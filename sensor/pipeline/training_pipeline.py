7a65fd2c7df862f1c8eca2612add3692c1c49efc
wilsoncharles <wilsoncharles21@gmail.com>
2022-11-12 09:44:35 +0000

    deploy

--- sensor/pipeline/training_pipeline.py
+++ sensor/pipeline/training_pipeline.py
@@ -110,7 +110,7 @@ class TrainPipeline:
             model_pusher_artifact:ModelPusherArtifact = self.start_model_pusher(model_eval_artifact)
             TrainPipeline.is_pipeline_running = False
             self.sync_artifact_dir_to_s3()
-            self.sync_saved_model_dir_to_s3()
+            self.sync_model_dir_to_s3()
         except Exception as e:
             self.sync_artifact_dir_to_s3()
             TrainPipeline.is_pipeline_running= False
